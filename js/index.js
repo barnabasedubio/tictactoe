5620dc852f8bc312662d50f698a9a975d605cecb
Barnabas Edubio <barny.edubio@gmail.com>
2017-11-30 00:55:05 +0100

    added functionality to add alternating symbols on click

--- js/index.js
+++ js/index.js
@@ -1,11 +1,35 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     console.log("hey");
-    var tilt = require("vanilla-tilt");
+    const tilt = require("vanilla-tilt"); // importing tiltJS
 
-    tilt.init(document.querySelectorAll(".tilt"), {
+    tilt.init(document.querySelectorAll(".tilt"), { // create tilt elements
         max: 25, // tilt angle
         speed: 60, // reaction speed
         scale: 1.15
-    })
+    });
+
+    let playerIsX = true;
+    let playerTurn = true;
+
+    let boxes = document.querySelectorAll(".row-box");
+    Array.prototype.forEach.call(boxes, function(el, i) {
+        el.addEventListener("click", function () {
+            if (el.children.length === 0) {
+                let symbol = document.createElement("div");
+                if (playerIsX) {
+                    symbol.setAttribute("class", "x-icon");
+                    el.appendChild(symbol);
+                    playerIsX = false;
+                } else {
+                    symbol.setAttribute("class", "o-icon");
+                    el.appendChild(symbol);
+                    playerIsX = true;
+                }
+            }
+        });
+    });
+
+    // landing page screen
+
 });
