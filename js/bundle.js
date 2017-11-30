51269302e6a4d6da21cb44918efb0d9f77a159c9
Barnabas Edubio <barny.edubio@gmail.com>
2017-11-30 22:11:18 +0100

    added game state array, board now locks up after player has made turn, must stll programm bot response

--- js/bundle.js
+++ js/bundle.js
@@ -69,38 +69,57 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("hey");
-    const tilt = __webpack_require__(1); // importing tiltJS
 
-    tilt.init(document.querySelectorAll(".tilt"), { // create tilt elements
+    let tiltElements = document.querySelectorAll(".tilt");
+
+    const tilt = __webpack_require__(1); // importing tiltJS
+    tilt.init(tiltElements, { // create tilt elements
         max: 25, // tilt angle
-        speed: 60, // reaction speed
+        speed: 60, // zoom-out speed
         scale: 1.15
     });
 
     let playerIsX = true;
     let playerTurn = true;
 
+    // create a 1x9 array representing  current game state
+    let gameStateArray = [];
+    gameStateArray.length = 9;
+    gameStateArray.fill(" ");
+
     let boxes = document.querySelectorAll(".row-box");
-    Array.prototype.forEach.call(boxes, function(el, i) {
-        el.addEventListener("click", function () {
-            if (el.children.length === 0) {
+    Array.prototype.forEach.call(boxes, function(box, i) {
+        box.addEventListener("click", function () {
+            if (!box.children.length && playerTurn) { // no symbols in the box
                 let symbol = document.createElement("div");
                 if (playerIsX) {
                     symbol.setAttribute("class", "x-icon");
-                    el.appendChild(symbol);
                     playerIsX = false;
                 } else {
                     symbol.setAttribute("class", "o-icon");
-                    el.appendChild(symbol);
                     playerIsX = true;
                 }
+                box.appendChild(symbol);
+                updateGameState(i, symbol.getAttribute("class"));
+                Array.prototype.forEach.call(tiltElements, function(tiltElement) {
+                    tiltElement.vanillaTilt.destroy(); // lock game board until CPU finished move
+                });
+                botTurn();
+                playerTurn = false;
             }
         });
     });
 
-    // landing page screen
+    function updateGameState(val, iconType) {
+        gameStateArray[val] = (iconType === "x-icon") ? "x" : "o";
+        console.log(gameStateArray)
+    }
+    // based on the game state, perform best possible move
+    function botTurn() {
 
+    }
+
+    // TODO: landing page screen
 });
 
 /***/ }),
