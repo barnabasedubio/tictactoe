d5c32eb8b22df1e55803b04fbc93cba326e75b1e
Barnabas Edubio <barny.edubio@gmail.com>
2017-12-03 01:50:35 +0100

    added some steps to the bot, it can now win in the most basic scenarios

--- js/bundle.js
+++ js/bundle.js
@@ -72,52 +72,152 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let tiltElements = document.querySelectorAll(".tilt");
 
-    const tilt = __webpack_require__(1); // importing tiltJS
-    tilt.init(tiltElements, { // create tilt elements
+    const tilt = __webpack_require__(1);
+    // initializing vanillaTilt
+    tilt.init(tiltElements, {
         max: 25, // tilt angle
         speed: 60, // zoom-out speed
         scale: 1.15
     });
 
-    let playerIsX = true;
-    let playerTurn = true;
+    /*
+    function toggleTilt(flag) {
+        let tiltElements = document.querySelectorAll(".tilt");
+        if (flag) {
+            tilt.init(tiltElements, { // create tilt elements
+                max: 25, // tilt angle
+                speed: 60, // zoom-out speed
+                scale: 1.15
+            });
+        } else {
+            Array.prototype.forEach.call(tiltElements, function(tiltElement) {
+                tiltElement.vanillaTilt.destroy(); // lock game board until bot finished move
+            });
+        }
+    } */
+
+    let playerBegins = false;
+    let playerTurn = false;
+
+    let moveRound = 0; // represents round one is in (or round a move was made)
 
     // create a 1x9 array representing  current game state
     let gameStateArray = [];
     gameStateArray.length = 9;
     gameStateArray.fill(" ");
 
+    let playerMoveArray = [];
+    let botMoveArray = [];
+
     let boxes = document.querySelectorAll(".row-box");
+
+    if (playerBegins) {
+        playerTurn = true;
+    } else {
+        botTurn();
+    }
+
+    // for the player
     Array.prototype.forEach.call(boxes, function(box, i) {
         box.addEventListener("click", function () {
             if (!box.children.length && playerTurn) { // no symbols in the box
                 let symbol = document.createElement("div");
-                if (playerIsX) {
+                if (playerBegins) {
                     symbol.setAttribute("class", "x-icon");
-                    playerIsX = false;
                 } else {
                     symbol.setAttribute("class", "o-icon");
-                    playerIsX = true;
                 }
                 box.appendChild(symbol);
-                updateGameState(i, symbol.getAttribute("class"));
-                Array.prototype.forEach.call(tiltElements, function(tiltElement) {
-                    tiltElement.vanillaTilt.destroy(); // lock game board until CPU finished move
-                });
-                botTurn();
+                updateGameState(symbol.getAttribute("class"), i);
+                // toggleTilt(false);
+                moveRound++;
                 playerTurn = false;
+                playerMoveArray.push(i);
+                botTurn()
             }
         });
     });
 
-    function updateGameState(val, iconType) {
-        gameStateArray[val] = (iconType === "x-icon") ? "x" : "o";
+    function updateGameState(iconType, index) {
+        gameStateArray[index] = (iconType === "x-icon") ? "x" : "o";
         console.log(gameStateArray)
     }
     // based on the game state, perform best possible move
     function botTurn() {
+        // if beginning of the match, place x in the center
+        if (!playerBegins) {
+            let icon = "x-icon";
+            switch (moveRound) {
+                case 0:
+                    placeSymbolAtIndex(icon, 4);
+                    botMoveArray.push(4);
+                    break;
+                case 2:
+                    let indexOfPlayer = parseInt(getIndex());
+                    if (indexOfPlayer % 2 === 1) {
+                        // player placed o in a side field -> will lose match
+                        placeSymbolAtIndex(icon, (indexOfPlayer + 5) % 10);
+                        botMoveArray.push((indexOfPlayer + 5) % 10);
+                    } else {
+                        // player placed o in diagonal field -> can still tie match
+                        placeSymbolAtIndex(icon, 16 - (indexOfPlayer + 8)); // diagonally away from player
+                        botMoveArray.push(16 - (indexOfPlayer + 8));
+                    }
+                    break;
+                case 4:
+                    // check if in a match point position
+                    if (checkIfEmpty(12 - (botMoveArray[0] + botMoveArray[1]))) {
+                        placeSymbolAtIndex(icon, 12 - (botMoveArray[0] + botMoveArray[1]));
+                        gameOver();
+                    }
+                    else {
 
+                    }
+                    // TODO check if player is in a match-point-position and diffuse if necessary
+
+
+            }
+        } else {
+            let icon = "o-icon";
+        }
+        // toggleTilt(true);
+        playerTurn = true;
+        moveRound++;
     }
+
+    function checkIfEmpty(index) {
+        console.log("in checkifempty... index: " + index);
+        // check if box element corresponding to index has a symbol in it
+        if (boxes[index].children.length === 0) {
+            console.log("we win!");
+            return true;
+        }
+        console.log("already occupied");
+        return false;
+    }
+
+    function placeSymbolAtIndex(iconType, index) {
+        let targetBox = boxes[index];
+        console.log("index: " + index);
+        if (!targetBox.children.length) {
+            let symbol = document.createElement("div");
+            symbol.setAttribute("class", iconType);
+            targetBox.appendChild(symbol);
+            updateGameState(iconType, index)
+        } else {
+            alert("attempt to place symbol at " + index + " failed");
+        }
+    }
+
+    function getIndex() {
+        for (let i in gameStateArray) {
+            if (gameStateArray[i] === "o") {
+                return i;
+            }
+        }
+    }
+
+    function gameOver() {}
 
     // TODO: landing page screen
 });
--- js/index.js
+++ js/index.js
@@ -3,16 +3,32 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let tiltElements = document.querySelectorAll(".tilt");
 
-    const tilt = require("vanilla-tilt"); // importing tiltJS
-    tilt.init(tiltElements, { // create tilt elements
+    const tilt = require("vanilla-tilt");
+    // initializing vanillaTilt
+    tilt.init(tiltElements, {
         max: 25, // tilt angle
         speed: 60, // zoom-out speed
         scale: 1.15
     });
 
-    let playerBegins = true;
-    let playerIsX = true;
-    let playerTurn = true;
+    /*
+    function toggleTilt(flag) {
+        let tiltElements = document.querySelectorAll(".tilt");
+        if (flag) {
+            tilt.init(tiltElements, { // create tilt elements
+                max: 25, // tilt angle
+                speed: 60, // zoom-out speed
+                scale: 1.15
+            });
+        } else {
+            Array.prototype.forEach.call(tiltElements, function(tiltElement) {
+                tiltElement.vanillaTilt.destroy(); // lock game board until bot finished move
+            });
+        }
+    } */
+
+    let playerBegins = false;
+    let playerTurn = false;
 
     let moveRound = 0; // represents round one is in (or round a move was made)
 
@@ -21,26 +37,34 @@ document.addEventListener("DOMContentLoaded", function () {
     gameStateArray.length = 9;
     gameStateArray.fill(" ");
 
+    let playerMoveArray = [];
+    let botMoveArray = [];
+
     let boxes = document.querySelectorAll(".row-box");
+
+    if (playerBegins) {
+        playerTurn = true;
+    } else {
+        botTurn();
+    }
+
+    // for the player
     Array.prototype.forEach.call(boxes, function(box, i) {
         box.addEventListener("click", function () {
             if (!box.children.length && playerTurn) { // no symbols in the box
                 let symbol = document.createElement("div");
-                if (playerIsX) {
+                if (playerBegins) {
                     symbol.setAttribute("class", "x-icon");
-                    playerIsX = false;
                 } else {
                     symbol.setAttribute("class", "o-icon");
-                    playerIsX = true;
                 }
                 box.appendChild(symbol);
-                updateGameState(i, symbol.getAttribute("class"));
-                Array.prototype.forEach.call(tiltElements, function(tiltElement) {
-                    tiltElement.vanillaTilt.destroy(); // lock game board until CPU finished move
-                });
+                updateGameState(symbol.getAttribute("class"), i);
+                // toggleTilt(false);
                 moveRound++;
-                botTurn();
                 playerTurn = false;
+                playerMoveArray.push(i);
+                botTurn()
             }
         });
     });
@@ -57,28 +81,55 @@ document.addEventListener("DOMContentLoaded", function () {
             switch (moveRound) {
                 case 0:
                     placeSymbolAtIndex(icon, 4);
+                    botMoveArray.push(4);
                     break;
                 case 2:
-                    if (gameStateArray[1] === "o" || gameStateArray[3] === "o" ||
-                        gameStateArray[5] === "o" || gameStateArray[7] === "o") {
+                    let indexOfPlayer = parseInt(getIndex());
+                    if (indexOfPlayer % 2 === 1) {
                         // player placed o in a side field -> will lose match
-
+                        placeSymbolAtIndex(icon, (indexOfPlayer + 5) % 10);
+                        botMoveArray.push((indexOfPlayer + 5) % 10);
                     } else {
                         // player placed o in diagonal field -> can still tie match
+                        placeSymbolAtIndex(icon, 16 - (indexOfPlayer + 8)); // diagonally away from player
+                        botMoveArray.push(16 - (indexOfPlayer + 8));
+                    }
+                    break;
+                case 4:
+                    // check if in a match point position
+                    if (checkIfEmpty(12 - (botMoveArray[0] + botMoveArray[1]))) {
+                        placeSymbolAtIndex(icon, 12 - (botMoveArray[0] + botMoveArray[1]));
+                        gameOver();
+                    }
+                    else {
 
                     }
+                    // TODO check if player is in a match-point-position and diffuse if necessary
+
 
             }
         } else {
             let icon = "o-icon";
         }
-
+        // toggleTilt(true);
         playerTurn = true;
         moveRound++;
     }
 
+    function checkIfEmpty(index) {
+        console.log("in checkifempty... index: " + index);
+        // check if box element corresponding to index has a symbol in it
+        if (boxes[index].children.length === 0) {
+            console.log("we win!");
+            return true;
+        }
+        console.log("already occupied");
+        return false;
+    }
+
     function placeSymbolAtIndex(iconType, index) {
         let targetBox = boxes[index];
+        console.log("index: " + index);
         if (!targetBox.children.length) {
             let symbol = document.createElement("div");
             symbol.setAttribute("class", iconType);
@@ -88,6 +139,16 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("attempt to place symbol at " + index + " failed");
         }
     }
+
+    function getIndex() {
+        for (let i in gameStateArray) {
+            if (gameStateArray[i] === "o") {
+                return i;
+            }
+        }
+    }
+
+    function gameOver() {}
 
     // TODO: landing page screen
 });
